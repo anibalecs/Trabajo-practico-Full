4968c8c955f9999119c573c293e19625b3ba1ae3
Anibal Contreras <anibalecs@gmail.com>
2024-03-21 13:59:29 -0300

    comienzo de estructura con get y post

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require("express"); 
 const app = express();
+app.use(express.json());
 const http = require("http").createServer(app);
 require('dotenv').config(); 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+app.get("/muñeco", (req, res) => {
     res.send("Hola mundo.");
+    let resultado = {
+       'Oso':{'precio':20500, 'medida':70, 'color':'', 'accesorios':''},
+       'Conejo':{'precio':12000, 'medida':30, 'color':'', 'accesorios':''},
+       'Perro':{'precio':15700, 'medida':50, 'color':'', 'accesorios':''},
+       'Mapache':{'precio':18000, 'medida':60, 'color':'', 'accesorios':''},
+       'Gato':{'precio':14200, 'medida':40, 'color':'', 'accesorios':''}
+    }
+    res.json({'muñeco':resultado})
 })
 
-app.post("/", (req, res)=>{
+app.get("/color", (req, res) => {
+    res.send("Hola mundo.");
+    let resultado = {
+       '1':'rosa', 
+       '2':'amarillo', 
+       '3':'verde'
+    }
+    res.json({'color':resultado})
+})
+
+app.get("/accesorios", (req, res) => {
+    res.send("Hola mundo.");
+    let resultado = {
+       '1':'camiseta y pelota de futbol',
+       '2':'guitarra electrica',
+       '3':'notebook',
+       '4':'sin accesorios'
+    }
+    res.json({'accesorios':resultado})
+})
+
+app.post("/signup", (req, res)=>{
+    res.end("Llamada post.")
+})
+
+app.post("/login", (req, res)=>{
     res.end("Llamada post.")
 })
 
