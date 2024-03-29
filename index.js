305776a09c59a67b81912ae79ff1a39289552de4
Anibal Contreras <anibalecs@gmail.com>
2024-03-29 13:15:58 -0300

    conectando con la base de datos

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const http = require("http").createServer(app);
 require('dotenv').config(); 
 const PORT = process.env.PORT || 8000;
 
-app.get("/muñeco", (req, res) => {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.urlMongoDb;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.post("/login", (req, res)=>{
+    const datosUsuario = req.body;
+    console.log(datosUsuario);
+    res.send(datosUsuario);
+})
+
+app.get("/categorias", (req, res) => {
     res.send("Hola mundo.");
     let resultado = {
        'Oso':{'precio':20500, 'medida':70, 'color':'', 'accesorios':''},
@@ -38,10 +69,8 @@ app.get("/accesorios", (req, res) => {
     res.json({'accesorios':resultado})
 })
 
-app.post("/login", (req, res)=>{
-    let datos = req.body;
-    console.log(datos);
-    res.json({'respuesta':'usuarios reado?'})
+app.post("/eleccion", (req, res)=>{
+    res.end('llamada post')
 })
 
 http.listen(PORT,()=>{
