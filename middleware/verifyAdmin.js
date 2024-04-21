7c7381dcab7547760ff698f4fc1663bf38112a82
Anibal Contreras <anibalecs@gmail.com>
2024-04-21 17:23:05 -0300

    agregando middleware admin y perfeccionando detalles

---
+++ middleware/verifyAdmin.js
@@ -0,0 +1,11 @@
+const verifyAdmin = async (req, res, next) => {
+    const user = req.user;
+    if(user.roles != 'admin') {
+        res.status(401).send("No autorizado")
+    } else{
+         next();
+    }
+} 
+
+
+module.exports = {verifyAdmin}
